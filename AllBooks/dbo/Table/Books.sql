ca561e2cfd3952ed0196f653d70c38cdb97054e6
Daniel Dimitrov <46860664+DannyDimitrov2002@users.noreply.github.com>
2020-05-27 10:22:31 +0300

    Add files via upload

---
+++ AllBooks/dbo/Table/Books.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Books] (
+    [Id]     INT         NOT NULL PRIMARY KEY IDENTITY,
+    [Title]  NCHAR (30)  NOT NULL,
+    [Author] NCHAR (30)  NOT NULL,
+    [Genre]  NCHAR (30)  NOT NULL,
+    [Year]   INT         NOT NULL,
+    [Des]    NCHAR (500) NOT NULL,
+    [Photo]  NCHAR (20)  NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ DataLibrary/BusinessLogic/BookProcessor.cs
@@ -0,0 +1,37 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.BusinessLogic
+{
+    public static class BookProcessor
+    {
+        public static int CreateBook(string Title, string Author, string Genre, int Year, string Des, string Photo)
+        {
+            AllBooks data = new AllBooks
+            {
+                Title = Title,
+                Author = Author,
+                Genre = Genre,
+                Year = Year,
+                Des = Des,
+                Photo = Photo
+            };
+
+            string sql = @"insert int dbo.Books (Title, Author, Genre, Year, Des, Photo)
+                           values (@Title, @Author, @Genre, @Year, @Des, @Photo)";
+
+            return SqlDataAccess.SaveData(sql, data);
+        }
+        public static List<AllBooks> LoadBooks()
+        {
+            string sql = @"select Id, Title, Author, Genre, Year, Des, Photo
+                        from dbo.Books;";
+            return SqlDataAccess.LoadData<AllBooks>(sql);
+        }
+    }
+}
