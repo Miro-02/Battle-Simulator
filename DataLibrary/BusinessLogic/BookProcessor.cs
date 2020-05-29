3f62e81ca8a142a402e568e38dd5734112c13a33
Daniel Dimitrov <46860664+DannyDimitrov2002@users.noreply.github.com>
2020-05-29 23:31:24 +0300

    Add files via upload

--- DataLibrary/BusinessLogic/BookProcessor.cs
+++ DataLibrary/BusinessLogic/BookProcessor.cs
@@ -10,6 +10,7 @@ namespace DataLibrary.BusinessLogic
 {
     public static class BookProcessor
     {
+        //For creting a new book
         public static int CreateBook(string Title, string Author, string Genre, int Year, string Des, string Photo)
         {
             AllBooks data = new AllBooks
@@ -21,53 +22,95 @@ namespace DataLibrary.BusinessLogic
                 Des = Des,
                 Photo = Photo
             };
+            //sql query for adding inputed varialbles into the table
+            string sql = @"insert into dbo.Books (Title, Author, Genre, Year, Des, Photo)
+                           values (@Title, @Author, @Genre, @Year, @Des, @Photo)";
 
-            string sql = @"insert into dbo.Books (Id, Title, Author, Genre, Year, Des, Photo)
-                           values (5, @Title, @Author, @Genre, @Year, @Des, @Photo)";
-
+            //Using function for saving into the database 
             return SqlDataAccess.SaveData(sql, data);
         }
+
+        //For reading from the database 
         public static List<AllBooks> LoadBooks()
         {
+            //sql query for selecting (reading) from the book table
             string sql = @"select *
-                        from dbo.Books;";
+                        from dbo.Books ORDER BY Title ASC;";
+            //Using function for reading from the book table
             return SqlDataAccess.LoadData<AllBooks>(sql);
         }
 
-
+        //For reading only books with Genre Biography
         public static List<AllBooks> LoadBiography()
         {
+            //sql query for reading only Biography books
             string sql = @"select *
                         from dbo.Books where Genre = 'Biography';";
+            //Using functions to celect from the table with our query
             return SqlDataAccess.LoadData<AllBooks>(sql);
         }
 
 
+        //For reading only books with Genre Comedy
         public static List<AllBooks> LoadComedy()
         {
             string sql = @"select *
                         from dbo.Books where Genre = 'Comedy';";
+            //Using functions to celect from the table with our query
             return SqlDataAccess.LoadData<AllBooks>(sql);
         }
 
+        //For reading only books with Genre Horror
         public static List<AllBooks> LoadHorror()
         {
             string sql = @"select *
                         from dbo.Books where Genre = 'Horror';";
+            //Using functions to celect from the table with our query
             return SqlDataAccess.LoadData<AllBooks>(sql);
         }
 
+        //For reading only books with Genre Fantasy
         public static List<AllBooks> LoadFantasy()
         {
             string sql = @"select *
                         from dbo.Books where Genre = 'Fantasy';";
+            //Using functions to celect from the table with our query
             return SqlDataAccess.LoadData<AllBooks>(sql);
         }
 
+        //For reading only books with Genre Tragedy
         public static List<AllBooks> LoadTragedy()
         {
             string sql = @"select *
                         from dbo.Books where Genre = 'Tragedy';";
+            //Using functions to celect from the table with our query
+            return SqlDataAccess.LoadData<AllBooks>(sql);
+        }
+
+        //For reading only new books(released after 2015)
+        public static List<AllBooks> NewBooks()
+        {
+            string sql = @"select *
+                        from dbo.Books where Year >= 2015;";
+            //Using functions to celect from the table with our query
+            return SqlDataAccess.LoadData<AllBooks>(sql);
+        }
+
+        //For reading only new classic(released before 1900)
+        public static List<AllBooks> ClassicBooks()
+        {
+            string sql = @"select *
+                        from dbo.Books where Year <= 1900;";
+            //Using functions to celect from the table with our query
+            return SqlDataAccess.LoadData<AllBooks>(sql);
+        }
+
+        //For reading the database with alphabeticlly ordered Authors
+        public static List<AllBooks> AuthorsSort()
+        {
+            string sql = @"select *
+                        from dbo.Books ORDER BY Author ASC;";
+            //Using functions to celect from the table with our query
             return SqlDataAccess.LoadData<AllBooks>(sql);
         }
     }
--- DataLibrary/DataAccess/SqlDataAccess.cs
+++ DataLibrary/DataAccess/SqlDataAccess.cs
@@ -12,11 +12,14 @@ namespace DataLibrary.DataAccess
 {
     public static class SqlDataAccess
     {
+        //Connecting to the database Books
         public static string GetConnectionString(string connectionName = "Books")
         {
             return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
         }
 
+
+        //Load the database 
         public static List<T> LoadData<T>(string sql)
         {
             using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
@@ -25,6 +28,7 @@ namespace DataLibrary.DataAccess
             }
         }
 
+        //Save into the database
         public static int SaveData<T>(string sql, T data)
         {
             using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
